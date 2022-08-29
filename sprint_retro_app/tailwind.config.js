f477c3c39ebd4b34c1caece9eb3d551d2dc1ca06
Samuel Tucker <Samuel.Tucker@generac.com>
2022-08-29 14:31:09 -0500

    Tailwind Setup

--- sprint_retro_app/tailwind.config.js
+++ sprint_retro_app/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./public/index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
