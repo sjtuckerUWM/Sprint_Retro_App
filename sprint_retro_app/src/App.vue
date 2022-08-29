b91cfbe9db8dadd781a91767770719f3ebe4b01b
Samuel Tucker <Samuel.Tucker@generac.com>
2022-08-29 13:57:43 -0500

    Figuring Out Structure

--- sprint_retro_app/src/App.vue
+++ sprint_retro_app/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <RetroApp/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RetroApp from './components/RetroApp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RetroApp
   }
 }
 </script>
--- sprint_retro_app/src/components/RetroApp.vue
+++ sprint_retro_app/src/components/RetroApp.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg }} bs</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -32,9 +32,8 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'RetroApp',
   props: {
-    msg: String
   }
 }
 </script>
